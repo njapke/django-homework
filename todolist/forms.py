e86382c91ebaf322d5ca1fcbe4a06178d558134d
alez <alexander.zoub@gmail.com>
2017-05-10 22:00:12 +0200

    Todo-list no longer called 'example-list'. Values under 0 and over 100 for completion are no longer permitted

--- todolist/forms.py
+++ todolist/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class TodoForm(forms.Form):
     task = forms.CharField(label='Task', max_length=160,widget=forms.TextInput(attrs={'class' : 'form-control'}))
     date = forms.DateTimeField(label='Due',widget=forms.TextInput(attrs={'class' : 'form-control'}),input_formats=['%Y-%m-%d %H:%M'])
-    completion = forms.IntegerField(label="Completion",widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    completion = forms.IntegerField(label="Completion",widget=forms.NumberInput(attrs={'class' : 'form-control','min':'0','max':"100"}))
 
 
 class DeleteForm(forms.Form):
